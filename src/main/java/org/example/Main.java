217bceb12a92514aa481d8354b8910a172eb2551
mmunkyeong <jmk4379@naver.com>
2023-02-23 13:49:36 +0900

    class 상속 예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
+//02.23
+// 문제 : 아래와 같이 출력되도록 해주세요.
+// 조건 : `숨쉬다` 기능을 중복하지 말아주세요.
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person aPerson = new Person();
+        aPerson.breath();
     }
+}
+class Animal{
+    void breath(){
+        System.out.println("숨쉬다.");
+    }
+}
+class Person extends Animal{
+
 }
